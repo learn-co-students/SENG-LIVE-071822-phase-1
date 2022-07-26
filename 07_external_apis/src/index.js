c2bf3bff4bf95117f4aa0cc9b3dff575124424e3
Louis <Louis.Medina@flatironschool.com>
2022-07-26 12:34:13 -0500

    add minor changes for 07 lecture

--- 07_external_apis/src/index.js
+++ 07_external_apis/src/index.js
@@ -6,7 +6,8 @@ document.addEventListener('DOMContentLoaded', () => {
             .then(res => res.json())
         }
 
-        function createResources(url, body){
+        // Function for making a POST Request
+        function createResource(url, body){
             return fetch(url,{
                 method: 'POST', 
                 headers: {
@@ -16,6 +17,28 @@ document.addEventListener('DOMContentLoaded', () => {
             })
             .then(res => res.json())
         }
+
+        // Function for making a PATCH request
+        function updateResource(url, body){
+            return fetch(url,{
+                method: 'PATCH', 
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(body),
+            })
+            .then(res => res.json())
+        }
+
+        // Function for making a DELETE request
+        function deleteResource(url){
+            return fetch(url,{
+                method: 'DELETE'
+            })
+            .then(res => res.json())
+        }
+
+        // updateResource('http://localhost:3000/books/1', {price: 100})
 
     // Rendering functions
         // Renders Header
@@ -35,6 +58,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const h3 = document.createElement('h3')
             const pAuthor = document.createElement('p')
             const pPrice = document.createElement('p')
+            const pInventory = document.createElement('input')
             const img = document.createElement('img')
             const btn = document.createElement('button')
     
@@ -42,14 +66,17 @@ document.addEventListener('DOMContentLoaded', () => {
             pAuthor.textContent = cardData.author
             pPrice.textContent = `$${cardData.price}`
             btn.textContent = 'Delete'
-    
+            pInventory.value = cardData.inventory
+            pInventory.type = 'number'
+
             img.src = cardData.imageUrl
             li.className = 'list-li'
     
             //Event Listeners 
-            btn.addEventListener('click',()=>li.remove())
+            btn.addEventListener('click', () => handleDeleteBook(cardData.id, li))
+            pInventory.addEventListener('change', (e) => handleUpdateBook(cardData.id, e.target.value))
         
-            li.append(h3,pAuthor,pPrice,img,btn)
+            li.append(h3,pAuthor,pPrice,img,pInventory,btn)
             document.querySelector('#book-list').append(li)
         }
     
@@ -65,12 +92,27 @@ document.addEventListener('DOMContentLoaded', () => {
                 inventory:e.target.inventory.value,
                 reviews:[]
             }
-            createResources('http://localhost:3000/books', book)
+            createResource('http://localhost:3000/books', book)
             .then(renderBookCard)
             .catch(e => console.error(e))
 
         }
 
+        function handleUpdateBook(id, inventoryNum) {
+            updateResource(`http://localhost:3000/books/${id}`, {inventory: inventoryNum})
+            .catch(e => console.error(e))
+        }
+
+        function handleDeleteBook(id, bookCard) {
+            deleteResource(`http://localhost:3000/books/${id}`)
+            // Pessimistic Rendering
+            .then(() => bookCard.remove())
+            // additional asynchronous behaviors...
+            // .then(() => someFunction())
+            // .then(() => someOtherFunction())
+            .catch(e => console.error(e))
+        }
+    
         function handleRenderSearch(){
             document.querySelector('main').innerHTML = `
             <form id="api-Search">
@@ -82,15 +124,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
             document.querySelector('#api-search').addEventListener('submit', handleAPIQuery)
         }
+        
         //Handles Google Books API search
         function handleAPIQuery(e){
             e.preventDefault()
             const search = e.target.search.value
-               
+            
+            // use this API endpoint
+            // `https://www.googleapis.com/books/v1/volumes?q=${search}&maxResults=20&key=${apiKEY}`
         }
 
-    
-    
     // Invoking functions    
         fetchResource('http://localhost:3000/stores/1')
         .then(store => {
